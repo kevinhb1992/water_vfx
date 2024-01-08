47ce1f97c4b1bea7a4f869cfc8a0849e78195ba9
kevinhb92 <118169391+kevinhb1992@users.noreply.github.com>
2024-01-08 14:43:11 +0900

    updated: it falls down instead of throwing

--- Water/Assets/Water/WaterBall/WaterBall.cs
+++ Water/Assets/Water/WaterBall/WaterBall.cs
@@ -19,25 +19,23 @@ public class WaterBall : MonoBehaviour
     {
         float lerp = 0;
         Vector3 startPos = transform.position;
+        Vector3 endPos = new Vector3(startPos.x, target.y, startPos.z); // Set the end position to have the same x and z, but the y of the target
+
         while (lerp < 1)
         {
-            transform.position = Vector3.Lerp(startPos, target, _SpeedCurve.Evaluate(lerp));
-            float magnitude = (transform.position - target).magnitude;
-            if (magnitude < 0.4f)
-            {
-                break;
-            }
+            transform.position = Vector3.Lerp(startPos, endPos, _SpeedCurve.Evaluate(lerp));
             lerp += Time.deltaTime * _Speed;
             yield return null;
         }
+
         _WaterBallParticleSystem.Stop(false, ParticleSystemStopBehavior.StopEmittingAndClear);
-        ParticleSystem splas = Instantiate(_SplashPrefab, target, Quaternion.identity);
-        Vector3 forward = target - startPos;
+        ParticleSystem splas = Instantiate(_SplashPrefab, endPos, Quaternion.identity);
+        Vector3 forward = endPos - startPos;
         forward.y = 0;
         splas.transform.forward = forward;
-        if (Vector3.Angle(startPos - target, Vector3.up) > 30)
+        if (Vector3.Angle(startPos - endPos, Vector3.up) > 30)
         {
-            ParticleSystem spill = Instantiate(_SpillPrefab, target, Quaternion.identity);
+            ParticleSystem spill = Instantiate(_SpillPrefab, endPos, Quaternion.identity);
             spill.transform.forward = forward;
         }
         Destroy(gameObject, 0.5f);
